6ccf1241cd1aaed35ec31f7673d3c0680bcd42b4
Tshepo123456 <tshepomashego678@gmail.com>
2025-07-03 23:49:59 +0200

    python is not that bad

---
+++ strings.py
@@ -0,0 +1,10 @@
+#advanced concepts - strings
+
+message =' Hello, world! '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # convert all character to lowercase
+print(message.split(',')) # split the string into a list based on the comma
+
+#upper method
+#replace method 
